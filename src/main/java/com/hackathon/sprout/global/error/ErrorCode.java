21ce1051de2b82cb951afda847ebcdc2260471ab
이서빈 <bommy00@naver.com>
2024-07-18 01:00:01 +0900

    feat: Token과 관련된 ErrorCode 작성

--- src/main/java/com/hackathon/sprout/global/error/ErrorCode.java
+++ src/main/java/com/hackathon/sprout/global/error/ErrorCode.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다."),
+    INVALID_TOKEN(401, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(403, "EXPIRED_TOKEN", "만료된 토큰입니다.");
 
     private final int httpStatus;
     private final String code;
