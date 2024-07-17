6fd44bf9804510a7e93736a29eaeefe34551f3d0
이서빈 <bommy00@naver.com>
2024-07-18 00:56:26 +0900

    feat: ErrorCode 열거형 클래스 작성
    * 에러 코드를 한 곳에 모아 정리할 수 있게 하기 위해서 만들었습니다.
    * 추후 수정할 수 있습니다...!

---
+++ src/main/java/com/hackathon/sprout/global/error/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.hackathon.sprout.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다."),
+
+    private final int httpStatus;
+    private final String code;
+    private final String message;
+}
