c5b64d6b2caf0d7fa938d7745462800190f0c8e3
한수한 <hshhan1009@naver.com>
2024-07-18 22:43:49 +0900

    feat : 채팅 api 컨트롤러 구현

--- src/main/java/com/hackathon/sprout/domain/chat/controller/ChatController.java
+++ src/main/java/com/hackathon/sprout/domain/chat/controller/ChatController.java
@@ -1,7 +1,56 @@
 package com.hackathon.sprout.domain.chat.controller;
 
+import com.hackathon.sprout.domain.chat.domain.ChatMessage;
+import com.hackathon.sprout.domain.chat.dto.request.ChatMessageCreateRequest;
+import com.hackathon.sprout.domain.chat.dto.request.ChatRoomCreateRequest;
+import com.hackathon.sprout.domain.chat.dto.response.ChatMessageInitResponse;
+import com.hackathon.sprout.domain.chat.dto.response.ChatMessageResponse;
+import com.hackathon.sprout.domain.chat.dto.response.ChatRoomBasicInfoResponse;
+import com.hackathon.sprout.domain.chat.dto.response.ChatRoomResponse;
+import com.hackathon.sprout.domain.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/chat")
 public class ChatController {
+    private final ChatService chatService;
+
+    @GetMapping("/room")
+    public ResponseEntity<List<ChatRoomBasicInfoResponse>> getRoomList(@RequestParam String date){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(chatService.getChatRoomList(date).stream().map(ChatRoomBasicInfoResponse::new).toList());
+    }
+
+    @PostMapping("/room")
+    public ResponseEntity<ChatMessageInitResponse> createRoom(@RequestBody ChatRoomCreateRequest request){
+        ChatMessage chatMessage = chatService.createChatRoom(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ChatMessageInitResponse(chatMessage));
+    }
+
+    @GetMapping("/room/{roomId}")
+    public ResponseEntity<ChatRoomResponse> getRoom(@PathVariable Long roomId){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ChatRoomResponse(chatService.getChatRoom(roomId)));
+    }
+
+    @PostMapping("/message")
+    public ResponseEntity<ChatMessageResponse> createMessage(@RequestBody ChatMessageCreateRequest request){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ChatMessageResponse(chatService.saveChatMessage(request)));
+    }
+
+    @DeleteMapping("/room/{roomId}")
+    public ResponseEntity<Void> deleteRoom(@PathVariable Long roomId) {
+        chatService.deleteChatRoom(roomId);
+        return ResponseEntity.noContent().build();
+    }
 }
