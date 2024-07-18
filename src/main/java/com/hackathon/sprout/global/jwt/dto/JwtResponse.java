cae52a23125b35e0e609d36b9ed114e6d9552647
이서빈 <bommy00@naver.com>
2024-07-18 23:56:10 +0900

    refactor: JwtResponse에 Builder 어노테이션 추가

--- src/main/java/com/hackathon/sprout/global/jwt/dto/JwtResponse.java
+++ src/main/java/com/hackathon/sprout/global/jwt/dto/JwtResponse.java
@@ -1,8 +1,10 @@
 package com.hackathon.sprout.global.jwt.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder
 @Getter
 @AllArgsConstructor
 public class JwtResponse {
