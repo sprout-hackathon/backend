802ab23ab78d5910f4d9ced5c7d01f77cf889f10
이서빈 <bommy00@naver.com>
2024-07-18 23:31:51 +0900

    feat: 관련 ErrorCode 추가

--- src/main/java/com/hackathon/sprout/global/error/ErrorCode.java
+++ src/main/java/com/hackathon/sprout/global/error/ErrorCode.java
@@ -9,7 +9,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다."),
     INVALID_TOKEN(401, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
-    EXPIRED_TOKEN(403, "EXPIRED_TOKEN", "만료된 토큰입니다.");
+    EXPIRED_TOKEN(403, "EXPIRED_TOKEN", "만료된 토큰입니다."),
+    INVALID_NATION_CODE(400, "INVALID_NATION_CODE", "유효하지 않은 국가 코드입니다."),
+    INVALID_LANGUAGE_CODE(400, "INVALID_LANGUAGE_CODE", "유효하지 않은 언어 코드입니다."),
+    INVALID_HOSPITAL_ID(400, "INVALID_HOSPITAL_ID", "유효하지 않은 병원 ID입니다."),
+    USER_NOT_FOUND(404, "USER_NOT_FOUND", "사용자를 찾을 수 없습니다.");
+
 
     private final int httpStatus;
     private final String code;
