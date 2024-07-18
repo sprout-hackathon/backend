b39d0fc786711ef878e1a92e5bfea1b9311aeca4
이서빈 <bommy00@naver.com>
2024-07-18 22:33:48 +0900

    feat: domain 생성
    * Application, Board, Comment, Address, Hospital, Recruitment, User, WorkHistory 관련 domain을 생성하였습니다.

---
+++ src/main/java/com/hackathon/sprout/domain/hospital/domain/Address.java
@@ -0,0 +1,33 @@
+package com.hackathon.sprout.domain.hospital.domain;
+
+import com.hackathon.sprout.global.shared.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Address extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer addressId;
+
+    @Column(length = 20, nullable = false)
+    private String sido;
+
+    @Column(length = 20)
+    private String gunGu;
+
+    @Column(length = 100)
+    private String roadName;
+
+    @Column(length = 20)
+    private String buildingNumber;
+
+    @Column(length = 200)
+    private String detailAddress;
+}
---
+++ src/main/java/com/hackathon/sprout/domain/workhistory/domain/WorkHistory.java
@@ -0,0 +1,30 @@
+package com.hackathon.sprout.domain.workhistory.domain;
+
+import com.hackathon.sprout.domain.hospital.domain.Hospital;
+import com.hackathon.sprout.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WorkHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long workHistoryId;
+
+    @Column(nullable = false)
+    private Byte workDuration;
+
+    @ManyToOne
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospitalId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+}
