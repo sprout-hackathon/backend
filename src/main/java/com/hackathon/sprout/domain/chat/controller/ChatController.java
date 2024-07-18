c68e8fd448a8122a911b415d0a495a5bbcaa10d3
한수한 <hshhan1009@naver.com>
2024-07-19 00:39:53 +0900

    fix: chat, hospital controller REST로 변경

--- src/main/java/com/hackathon/sprout/domain/chat/controller/ChatController.java
+++ src/main/java/com/hackathon/sprout/domain/chat/controller/ChatController.java
@@ -11,12 +11,11 @@ import com.hackathon.sprout.domain.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/chats")
 public class ChatController {
