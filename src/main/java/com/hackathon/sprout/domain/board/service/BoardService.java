239f1dac04c2b9944135b04651d0e2967c2857c6
이서빈 <bommy00@naver.com>
2024-07-19 19:30:07 +0900

    feat: board 수정 관련 로직 구현

--- src/main/java/com/hackathon/sprout/domain/board/service/BoardService.java
+++ src/main/java/com/hackathon/sprout/domain/board/service/BoardService.java
@@ -2,6 +2,7 @@ package com.hackathon.sprout.domain.board.service;
 
 import com.hackathon.sprout.domain.board.domain.Board;
 import com.hackathon.sprout.domain.board.dto.BoardRequest;
+import com.hackathon.sprout.domain.board.exception.BoardNotFoundException;
 import com.hackathon.sprout.domain.board.repository.BoardRepository;
 import com.hackathon.sprout.domain.user.domain.User;
 import com.hackathon.sprout.domain.user.exception.UserNotFoundException;
@@ -9,6 +10,7 @@ import com.hackathon.sprout.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +18,7 @@ public class BoardService {
     private final UserRepository userRepository;
     private final BoardRepository boardRepository;
 
+    @Transactional
     public void createBoard(Authentication authentication, BoardRequest boardRequest) {
         String userId = (String) authentication.getPrincipal();
 
@@ -34,4 +37,35 @@ public class BoardService {
         boardRepository.save(board);
     }
 
+    @Transactional
+    public void updateBoard(Authentication authentication, Long boardId, BoardRequest boardRequest) {
+        String userId = (String) authentication.getPrincipal();
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(BoardNotFoundException::new);
+
+        board.setTitle(boardRequest.getTitle());
+        board.setCategory(boardRequest.getCategory());
+        board.setContent(boardRequest.getContent());
+        board.setIsAnonymous(boardRequest.getIsAnonymous());
+
+        boardRepository.save(board);
+    }
+
+    @Transactional
+    public void deleteBoard(Authentication authentication, Long boardId) {
+        String userId = (String) authentication.getPrincipal();
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(BoardNotFoundException::new);
+
+        board.setIsDeleted(true);
+        boardRepository.save(board);
+    }
 }
