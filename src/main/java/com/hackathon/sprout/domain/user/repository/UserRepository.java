13073e6a9cc1c20bd46de86b9856e0a162f1a668
한수한 <hshhan1009@naver.com>
2024-07-20 00:14:42 +0900

    fix : 회원 기본키로 조회 기능 수정

--- src/main/java/com/hackathon/sprout/domain/user/repository/UserRepository.java
+++ src/main/java/com/hackathon/sprout/domain/user/repository/UserRepository.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(String id);
-    Optional<User> findByUserId(int userId);
 }
