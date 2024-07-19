e1cd4df2084bd6081d94cdbd454e80a24a2513ce
이서빈 <bommy00@naver.com>
2024-07-19 19:30:18 +0900

    feat: board 수정, 삭제 API 구현

--- src/main/java/com/hackathon/sprout/domain/board/controller/BoardController.java
+++ src/main/java/com/hackathon/sprout/domain/board/controller/BoardController.java
@@ -3,12 +3,10 @@ package com.hackathon.sprout.domain.board.controller;
 import com.hackathon.sprout.domain.board.dto.BoardRequest;
 import com.hackathon.sprout.domain.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +18,17 @@ public class BoardController {
     public ResponseEntity<Void> createBoard(Authentication authentication, @RequestBody BoardRequest boardRequest) {
         boardService.createBoard(authentication, boardRequest);
         return ResponseEntity.status(201).build();
+    }
+
+    @PatchMapping("/{boardId}")
+    public ResponseEntity<Void> updateBoard(Authentication authentication, @PathVariable("boardId") Long boardId, @RequestBody BoardRequest boardRequest) {
+        boardService.updateBoard(authentication, boardId, boardRequest);
+        return ResponseEntity.status(200).build();
+    }
+
+    @DeleteMapping("/{boardId}")
+    public ResponseEntity<Void> deleteBoard(Authentication authentication, @PathVariable("boardId") Long boardId) {
+        boardService.deleteBoard(authentication, boardId);
+        return ResponseEntity.status(204).build();
     }
 }
