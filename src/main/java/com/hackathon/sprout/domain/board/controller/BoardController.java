379ec559082a962c29bffbce6c6380d34ad9dce5
이서빈 <bommy00@naver.com>
2024-07-19 19:59:36 +0900

    feat: 댓글 생성, 수정, 삭제 API 구현

--- src/main/java/com/hackathon/sprout/domain/board/controller/BoardController.java
+++ src/main/java/com/hackathon/sprout/domain/board/controller/BoardController.java
@@ -1,7 +1,10 @@
 package com.hackathon.sprout.domain.board.controller;
 
 import com.hackathon.sprout.domain.board.dto.BoardRequest;
+import com.hackathon.sprout.domain.board.dto.CommentRequest;
+import com.hackathon.sprout.domain.board.dto.CommentUpdateRequest;
 import com.hackathon.sprout.domain.board.service.BoardService;
+import com.hackathon.sprout.domain.board.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/boards")
 public class BoardController {
     private final BoardService boardService;
+    private final CommentService commentService;
 
     @PostMapping
     public ResponseEntity<Void> createBoard(Authentication authentication, @RequestBody BoardRequest boardRequest) {
@@ -29,6 +33,24 @@ public class BoardController {
     @DeleteMapping("/{boardId}")
     public ResponseEntity<Void> deleteBoard(Authentication authentication, @PathVariable("boardId") Long boardId) {
         boardService.deleteBoard(authentication, boardId);
+        return ResponseEntity.status(204).build();
+    }
+
+    @PostMapping("/comments")
+    public ResponseEntity<Void> createComment(Authentication authentication, @RequestBody CommentRequest commentRequest) {
+        commentService.createComment(authentication, commentRequest);
+        return ResponseEntity.status(201).build();
+    }
+
+    @PatchMapping("/comments/{commentId}")
+    public ResponseEntity<Void> updateComment(Authentication authentication, @PathVariable("commentId") Long commentId, @RequestBody CommentUpdateRequest commentUpdateRequest) {
+        commentService.updateComment(authentication, commentId, commentUpdateRequest);
+        return ResponseEntity.status(200).build();
+    }
+
+    @DeleteMapping("/comments/{commentId}")
+    public ResponseEntity<Void> deleteComment(Authentication authentication, @PathVariable("commentId") Long commentId) {
+        commentService.deleteComment(authentication, commentId);
         return ResponseEntity.status(204).build();
     }
 }
