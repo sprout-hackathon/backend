36f467c9ee093252700704687f44a387e66b522e
이서빈 <bommy00@naver.com>
2024-07-21 20:22:18 +0900

    feat: User 정보 조회 관련 API 구현

--- src/main/java/com/hackathon/sprout/domain/user/controller/UserController.java
+++ src/main/java/com/hackathon/sprout/domain/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.hackathon.sprout.domain.user.controller;
 
 import com.hackathon.sprout.domain.user.dto.UserLoginRequest;
 import com.hackathon.sprout.domain.user.dto.UserRegisterRequest;
+import com.hackathon.sprout.domain.user.dto.UserResponse;
 import com.hackathon.sprout.domain.user.dto.UserUpdateRequest;
 import com.hackathon.sprout.domain.user.service.UserService;
 import com.hackathon.sprout.global.jwt.dto.JwtResponse;
@@ -17,13 +18,13 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody UserRegisterRequest userRegisterRequest) {
+    public ResponseEntity<Void> register(@RequestBody UserRegisterRequest userRegisterRequest) {
         userService.register(userRegisterRequest);
         return ResponseEntity.status(201).build();
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserLoginRequest userLoginRequest) {
+    public ResponseEntity<JwtResponse> login(@RequestBody UserLoginRequest userLoginRequest) {
         JwtResponse jwtResponse = userService.login(userLoginRequest);
         return ResponseEntity.status(201).body(jwtResponse);
     }
@@ -38,5 +39,11 @@ public class UserController {
     public ResponseEntity<Void> deleteUser(Authentication authentication) {
         userService.deleteUser(authentication);
         return ResponseEntity.status(204).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<UserResponse> getUser(Authentication authentication) {
+        UserResponse userResponse = userService.getUser(authentication);
+        return ResponseEntity.status(200).body(userResponse);
     }
 }
