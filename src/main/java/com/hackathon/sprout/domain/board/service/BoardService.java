28e7c1832ad5f918a9e95e66e780492eac256eb3
이서빈 <bommy00@naver.com>
2024-07-21 22:55:30 +0900

    feat: 특정 게시글 조회 관련 로직 구현

--- src/main/java/com/hackathon/sprout/domain/board/service/BoardService.java
+++ src/main/java/com/hackathon/sprout/domain/board/service/BoardService.java
@@ -1,9 +1,13 @@
 package com.hackathon.sprout.domain.board.service;
 
 import com.hackathon.sprout.domain.board.domain.Board;
+import com.hackathon.sprout.domain.board.domain.Comment;
 import com.hackathon.sprout.domain.board.dto.BoardRequest;
+import com.hackathon.sprout.domain.board.dto.BoardResponse;
+import com.hackathon.sprout.domain.board.dto.CommentResponse;
 import com.hackathon.sprout.domain.board.exception.BoardNotFoundException;
 import com.hackathon.sprout.domain.board.repository.BoardRepository;
+import com.hackathon.sprout.domain.board.repository.CommentRepository;
 import com.hackathon.sprout.domain.user.domain.User;
 import com.hackathon.sprout.domain.user.exception.UserNotFoundException;
 import com.hackathon.sprout.domain.user.repository.UserRepository;
@@ -12,11 +16,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final UserRepository userRepository;
     private final BoardRepository boardRepository;
+    private final CommentRepository commentRepository;
 
     @Transactional
     public void createBoard(Authentication authentication, BoardRequest boardRequest) {
@@ -67,5 +75,18 @@ public class BoardService {
 
         board.setIsDeleted(true);
         boardRepository.save(board);
+    }
+
+    @Transactional(readOnly = true)
+    public BoardResponse getBoard(Long boardId) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(BoardNotFoundException::new);
+
+        List<Comment> comments = commentRepository.findAllByBoardId(board);
+        List<CommentResponse> commentResponses = comments.stream()
+                .map(CommentResponse::new)
+                .collect(Collectors.toList());
+
+        return new BoardResponse(board, commentResponses);
     }
 }
