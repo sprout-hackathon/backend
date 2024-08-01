794ea5b88292a2488005f453793839670fbc7a65
이서빈 <bommy00@naver.com>
2024-08-01 12:30:32 +0900

    feat: File 관련 API 구현

---
+++ src/main/java/com/hackathon/sprout/domain/file/controller/FileController.java
@@ -0,0 +1,32 @@
+package com.hackathon.sprout.domain.file.controller;
+
+import com.hackathon.sprout.domain.file.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/files")
+@RequiredArgsConstructor
+public class FileController {
+
+    private final FileService fileService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            fileService.saveFiles(List.of(file));
+            return ResponseEntity.status(HttpStatus.CREATED).body("파일 업로드 성공!!");
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일 업로드 실패!!");
+        }
+    }
+}
