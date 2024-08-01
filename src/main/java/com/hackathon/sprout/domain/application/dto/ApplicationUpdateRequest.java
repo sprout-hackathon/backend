530a2d33bc358e04f9c287aa0575586b12b9a47a
이서빈 <bommy00@naver.com>
2024-08-01 14:41:15 +0900

    docs: Application 관련 Dto Swagger 문서화

--- src/main/java/com/hackathon/sprout/domain/application/dto/ApplicationUpdateRequest.java
+++ src/main/java/com/hackathon/sprout/domain/application/dto/ApplicationUpdateRequest.java
@@ -1,15 +1,17 @@
 package com.hackathon.sprout.domain.application.dto;
 
 import com.hackathon.sprout.domain.application.enums.ApplicationState;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class ApplicationUpdateRequest {
+    @Schema(description = "지원 상태", example = "접수완료")
     ApplicationState state;
 
-    public void setState(String state){
+    public void setState(String state) {
         this.state = ApplicationState.fromName(state);
     }
 }
