8513a1e7a64b2a5faad31cc8525afcf60d9476bd
이서빈 <bommy00@naver.com>
2024-08-01 15:10:17 +0900

    docs: Language와 Nation 관련 Swagger 문서화

--- src/main/java/com/hackathon/sprout/domain/country/domain/Nation.java
+++ src/main/java/com/hackathon/sprout/domain/country/domain/Nation.java
@@ -1,6 +1,10 @@
 package com.hackathon.sprout.domain.country.domain;
 
-import jakarta.persistence.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
@@ -11,12 +15,15 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Nation {
     @Id
+    @Schema(description = "국가 코드", example = "CA")
     @Column(length = 5)
     String nationCode;
 
+    @Schema(description = "국가 원어 이름", example = "Canada")
     @Column(length = 100)
     String nationOriginName;
 
+    @Schema(description = "국가 이름", example = "'캐나다'")
     @Column(length = 100)
     String nationName;
 }
