95d8ddc43def8a09116eb7292b8d1f603055dc41
이서빈 <bommy00@naver.com>
2024-08-01 16:51:24 +0900

    refactor: SearchCondition 명 변경

--- src/main/java/com/hackathon/sprout/domain/chat/dto/ChatSearchCondition.java
+++ src/main/java/com/hackathon/sprout/domain/chat/dto/ChatSearchCondition.java
@@ -1,6 +1,16 @@
 package com.hackathon.sprout.domain.chat.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public record ChatSearchCondition(
-    String date
-){}
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatSearchCondition {
+    @Schema(description = "검색 조건 날짜", example = "2024-01-01")
+    String date;
+}
