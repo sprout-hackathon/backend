1502e84947be0b9fb9e4960280040ea8c8ee97bb
이서빈 <bommy00@naver.com>
2024-08-01 14:07:41 +0900

    docs: User 관련 Dto Swagger 문서화

--- src/main/java/com/hackathon/sprout/domain/user/dto/UserLoginRequest.java
+++ src/main/java/com/hackathon/sprout/domain/user/dto/UserLoginRequest.java
@@ -1,5 +1,6 @@
 package com.hackathon.sprout.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,6 +9,9 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserLoginRequest {
+    @Schema(description = "사용자 ID", example = "user123")
     private String id;
+
+    @Schema(description = "사용자 비밀번호", example = "password123")
     private String password;
 }
--- src/main/java/com/hackathon/sprout/domain/user/dto/UserUpdateRequest.java
+++ src/main/java/com/hackathon/sprout/domain/user/dto/UserUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.hackathon.sprout.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,10 +9,21 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserUpdateRequest {
+    @Schema(description = "사용자 닉네임", example = "nickname123")
     private String nickname;
+
+    @Schema(description = "국가 코드", example = "US")
     private String nationCode;
+
+    @Schema(description = "언어 코드", example = "EN")
     private String languageCode;
+
+    @Schema(description = "언어 능력", example = "5")
     private Byte proficiency;
+
+    @Schema(description = "인증 여부", example = "true")
     private Boolean hasCertification;
+
+    @Schema(description = "인증 코드", example = "cert123")
     private String certificationCode;
 }
