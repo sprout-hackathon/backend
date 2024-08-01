a1a87e4faea2b476b48eba7e363b6e19b2d7ace6
이서빈 <bommy00@naver.com>
2024-08-02 01:50:32 +0900

    chore: Cors 추가

--- src/main/java/com/hackathon/sprout/global/config/CorsConfig.java
+++ src/main/java/com/hackathon/sprout/global/config/CorsConfig.java
@@ -24,6 +24,8 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://127.0.0.1:5173",
                         "http://localhost:3000",
                         "http://127.0.0.1:3000",
+                        "https://localhost:3000",
+                        "https://127.0.0.1:3000",
                         "http://localhost:8080",
                         "http://127.0.0.1:8080",
                         "https://localhost:8081",
