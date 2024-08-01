9a7da38c5714e1c24e0f6fe02fef2592e2abbf8b
이서빈 <bommy00@naver.com>
2024-08-01 12:28:17 +0900

    feat: File 관련 Dto 생성

---
+++ src/main/java/com/hackathon/sprout/domain/file/domain/File.java
@@ -0,0 +1,42 @@
+package com.hackathon.sprout.domain.file.domain;
+
+import com.hackathon.sprout.domain.image.domain.ImageMessage;
+import com.hackathon.sprout.global.shared.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class File extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long fileId;
+
+    @Column
+    private String uploadFileName;
+
+    @Column(length = 3000)
+    private String url;
+
+    @Column(nullable = false)
+    private long fileSize;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "image_message_id")
+    ImageMessage imageMessage;
+
+    @Builder(builderMethodName = "createFileBuilder")
+    public File(ImageMessage imageMessage, String uploadFileName, String url, long fileSize) {
+        this.imageMessage = imageMessage;
+        this.uploadFileName = uploadFileName;
+        this.url = url;
+        this.fileSize = fileSize;
+
+        imageMessage.getFiles().add(this);
+    }
+}
